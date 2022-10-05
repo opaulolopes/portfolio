1f5f25a8ce6776497824a369660ffed0ff99118a
Paulo Lopes <paulopesfilho@gmail.com>
2022-10-05 15:34:52 -0300

    portfolio prototype v1.0

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function clickMenu {
+const modal = document.getElementById('modal-aboutme')
+const btn = document.getElementById('about-me')
+const close = document.getElementById('close')
 
+btn.onclick = function() {
+    modal.style.display = 'block'
+}
+
+close.onclick = function() {
+    modal.style.display = 'none'
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none'
+    }
 }
