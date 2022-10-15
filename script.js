2dc90e5c8ebf162be66d0cb1038ad65516e8f643
Paulo Lopes <paulopesfilho@gmail.com>
2022-10-15 16:12:51 -0300

    portfolio update
    1. Webkit scrollbar added;
    2. Smooth scroll for internal links added;
    3. Intro picture and message sizes linked to VW on mobile and to VH on other displays;
    4. Footer overlap fixed on mobile;
    5. Making form accessible;
    7. Resizable text verified.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const toggle = document.getElementById('menu-toggle')
-
-window.onclick = function(event) {
-    if (event.target == toggle) {
-        toggle.display = 'none'
-    }
+var wheelDistance = function(evt) {
+      
+    // wheelDelta inidicates how 
+    // Far the wheel is turned
+    var w = evt.wheelDelta;
+          
+    // Returning normalized value
+    return w / 1;
 }
+  
+// Adding event listener for some element
+somEl.addEventListener("mousewheel", wheelDistance);
